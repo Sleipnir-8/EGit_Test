30a8ce06bc70099b371bc6dce0b1e18454f0e53a
Sleipnir-8 <63652861+Sleipnir-8@users.noreply.github.com>
2020-10-15 21:52:41 +0800

    Delete Sample2.java

--- src/Sample2.java
+++
@@ -1,8 +0,0 @@
-public class Sample2 {
-	
-	public static void main(String[] args) {
-		System.out.println("Programming is fun!");
-		System.out.println("Fandamentals First!");
-		System.out.println("Problem Driven");
-		}
-}
