c5b23c912ffccab57e65e0d5733cad58bbf1d771
Sleipnir-8 <63652861+Sleipnir-8@users.noreply.github.com>
2020-10-15 21:45:09 +0800

    Delete Sample6.java

--- src/Wrong/Sample6.java
+++
@@ -1,7 +0,0 @@
-public class Sample6 {
-	public static void main(String[] args) {
-		 
-		System.out.print("Celsius 35 is Fahrenheit degree ");
-		System.out.println("(9 / 5) * 35 + 32");
-	    }
-}
